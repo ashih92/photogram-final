0d85066e6d2edc8c26788996555e94b107882579
ashih92 <146589396+ashih92@users.noreply.github.com>
2023-12-09 05:36:54 +0000

    after generating resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_09_053230) do
+  create_table "users", force: :cascade do |t|
+    t.integer "comments_count"
+    t.string "email"
+    t.string "encrypted_password"
+    t.integer "likes_count"
+    t.boolean "private"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
